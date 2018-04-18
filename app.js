33eacf6a02f086372376f55ce9f49595edc94258
antoniotex <acm.skt@gmail.com>
2018-04-18 17:50:34 -0300

    Configurado dependências

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+app.use(bodyParser.json());
+app.use(cors());
+
+//Permite encontrar conteúdo estático
+app.use(express.static(__dirname + '/public'));
+
+//Cria entrada do banco de dados
+app.get('/new/:urlToShorten(*)', (req, res, next) => {
+    var { urlToShorten } = req.params;
+    console.log(urlToShorten)
+});
+
+
+
+
+
+//Listen to see if everything os working
+//Process is for if Heroku
+app.listen(process.env.PORT || 3000, () =>{
+    console.log('Tudo normal :)')
+});
